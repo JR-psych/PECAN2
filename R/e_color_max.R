eac571e2cdab3ffa2f7cf00092db156579e43e18
JR-psych <113672965+JR-psych@users.noreply.github.com>
2024-09-25 07:19:18 +0200

    Add warning message for wrong input

--- R/e_color_max.R
+++ R/e_color_max.R
@@ -2,6 +2,7 @@ e_color_max <- function(net_edges, edges_color_max){
 
   if(!is.numeric(edges_color_max) & !is.null(edges_color_max)) {stop("edges_color_max must be numeric or NULL")}
   if(is.null(edges_color_max)){edges_color_max <- as.numeric(max(net_edges$color))}
+  if(edges_color_max < max(net_edges$color)){stop("edges_color_max must be greater or equal than the maximum value of edges_color_by")}
   if(edges_color_max <= 0){stop("edges_color_max must be greater than 0")}
   if(edges_color_max < 10){net_edges$color <- net_edges$color * (10/edges_color_max)}
   if(edges_color_max > 10){net_edges$color <- net_edges$color / (edges_color_max/10)}
