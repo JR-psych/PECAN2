707c5355f6388960ce270be332fa340d5c86d499
JR-psych <113672965+JR-psych@users.noreply.github.com>
2024-09-25 07:27:52 +0200

    Added warning message. Dplyr not need anymore

--- R/vis_nodes_color_max.R
+++ R/vis_nodes_color_max.R
@@ -3,11 +3,12 @@
 #'
 #'
 #' @param vis.env
-#' @import dplyr
+#'
 #' @return
 #'
 #' @examples
 vis_nodes_color_max <- function(net_nodes,nodes_color_max){
+  if(nodes_color_max < max(net_nodes$color)){stop("nodes_color_max must be greater or equal than the maximum value of nodes_color")}
   if(nodes_color_max != 10){
    if(nodes_color_max < 10){net_nodes$color <- net_nodes$color * (10/nodes_color_max)}
     if(nodes_color_max > 10){net_nodes$color <- net_nodes$color / (nodes_color_max/10)}}
