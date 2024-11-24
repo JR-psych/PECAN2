83424646da2b45b467326a7c9fe695768022cd9e
JR-psych <113672965+JR-psych@users.noreply.github.com>
2024-11-24 18:29:28 +0100

    - added code to filter NA nodes

--- R/nodes_general_warnings.R
+++ R/nodes_general_warnings.R
@@ -6,5 +6,5 @@ if(is.data.frame(net_nodes) == FALSE){stop("nodes must be a dataframe")}
 #if("internal_color" %in% colnames(net_nodes)){warning("internal_color already exist in edges. This might cause problems")}
 if("id" %in% colnames(net_nodes) == FALSE){stop("id column in nodes is missing")}
 if(any(is.na(net_nodes$id))){stop("No Na´s allowed in nodes$id")}
-
+if(any(is.na(net_nodes$size))){stop("No Na´s allowed in nodes$size")}
 }
